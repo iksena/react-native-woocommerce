5c4d1dcb9ca5c219fc62b4fb4259404fa00cb5df
I Komang Sena <i.buwana@btpn.com>
2020-05-10 16:03:58 +0700

    Config index

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {persistor, store} from "./Redux";
+import NavigationStacks from "./Navigation/Stacks";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <NavigationContainer>
+                    <NavigationStacks/>
+                </NavigationContainer>
+            </PersistGate>
+        </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
