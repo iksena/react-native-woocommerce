a3164795472d6dd1692b0765c58f6c32960a8ab8
I Komang Sena <i.buwana@btpn.com>
2020-05-10 12:46:53 +0700

    Config redux

---
+++ src/Redux/index.js
@@ -0,0 +1,35 @@
+import { AsyncStorage } from 'react-native';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+import reducers from './Reducers';
+import rootSaga from './Sagas';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['auth']
+};
+
+// support debugging using react-native-debugger app
+// that supports redux devtools and component tree inspector
+// ref https://github.com/jhen0409/react-native-debugger
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const persistReducers = persistReducer(persistConfig, reducers);
+const store = createStore(persistReducers, composeEnhancers(applyMiddleware(logger, sagaMiddleware)));
+const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
+
+export {
+  store,
+  persistor
+};
