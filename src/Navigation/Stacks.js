cab2c7acd06d13d1a4ce5bd18bb161f8d9190be0
I Komang Sena <i.buwana@btpn.com>
2020-05-10 20:40:23 +0700

    Added tab navigator

--- src/Navigation/Stacks.js
+++ src/Navigation/Stacks.js
@@ -1,3 +1,4 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 
@@ -5,10 +6,19 @@ import RootContainer from '../Containers/Root/Root.container';
 import Routes from './Routes';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Home = () => (
+  <Tab.Navigator>
+    <Tab.Screen name={Routes.Browse} component={RootContainer}/>
+    <Tab.Screen name={Routes.Profile} component={RootContainer}/>
+  </Tab.Navigator>
+);
 
 const RootStack = () => (
-  <Stack.Navigator>
+  <Stack.Navigator initialRouteName={Routes.Root}>
     <Stack.Screen name={Routes.Root} component={RootContainer}/>
+    <Stack.Screen name={Routes.Home} component={Home}/>
   </Stack.Navigator>
 );
 
