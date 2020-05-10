79ce61725a7d4ed770fa10d9a0659fd1586d6819
I Komang Sena <i.buwana@btpn.com>
2020-05-10 20:40:03 +0700

    Added env

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,6 +1,11 @@
-import { WC_CONSUMER_KEY, WC_CONSUMER_SECRET } from 'react-native-dotenv';
+import {
+  WC_BASE_URL,
+  WC_CONSUMER_KEY,
+  WC_CONSUMER_SECRET
+} from 'react-native-dotenv';
 
 export default {
+  WC_BASE_URL,
   WC_CONSUMER_KEY,
   WC_CONSUMER_SECRET
 }
