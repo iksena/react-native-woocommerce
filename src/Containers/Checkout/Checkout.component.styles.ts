2eaae520e4a06efc4d9ca1911cb97dc728211f93
I Komang Sena <i.buwana@btpn.com>
2020-05-24 14:45:23 +0700

    Add orders to checkout

--- src/Containers/Checkout/Checkout.component.styles.ts
+++ src/Containers/Checkout/Checkout.component.styles.ts
@@ -1,12 +1,30 @@
-import { StyleSheet, ViewStyle } from 'react-native';
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 
 interface Styles {
-    container: ViewStyle;
+    checkoutContainer: ViewStyle;
+    textHeading: TextStyle;
+    textEmpty: TextStyle;
+    orderItem: ViewStyle;
 }
 
 const styles = StyleSheet.create<Styles>({
-  container: {
-    alignItems: 'center'
+  checkoutContainer: {
+    marginHorizontal: 5
+  },
+  textHeading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 10
+  },
+  orderItem: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 2
+  },
+  textEmpty: {
+    textAlign: 'center',
+    marginTop: 16
   }
 });
 
--- src/Containers/Checkout/Checkout.component.tsx
+++ src/Containers/Checkout/Checkout.component.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import { Formik } from 'formik';
 import { Button, Input } from 'react-native-elements';
 
-import { CartState } from '../../Models';
+import { CartItem, CartState } from '../../Models';
+import { toAmount } from '../../Utils';
+import styles from './Checkout.component.styles';
 
 interface Props extends CartState {
     handleCheckoutSubmit: (userInfo: object) => void;
@@ -30,55 +32,64 @@ const _renderForm = ({ handleCheckoutSubmit }: Props): JSX.Element => (
       handleBlur,
       handleSubmit,
       values
-    }) => (
-      <View>
+    }): JSX.Element => (
+      <>
         <Input
-          placeholder="First Name"
+          label="First Name"
+          placeholder="James"
           onChangeText={handleChange('first_name')}
           onBlur={handleBlur('first_name')}
           value={values.first_name}/>
         <Input
-          placeholder="Last Name"
+          label="Last Name"
+          placeholder="Moriarty"
           onChangeText={handleChange('last_name')}
           onBlur={handleBlur('last_name')}
           value={values.last_name}/>
         <Input
-          placeholder="Address 1"
+          label="Address 1"
+          placeholder="221B Baker Street"
           onChangeText={handleChange('address_1')}
           onBlur={handleBlur('address_1')}
           value={values.address_1}/>
         <Input
-          placeholder="Address 2"
+          label="Address 2"
           onChangeText={handleChange('address_2')}
           onBlur={handleBlur('address_2')}
           value={values.address_2}/>
         <Input
-          placeholder="City"
+          label="City"
+          placeholder="Birmingham"
           onChangeText={handleChange('city')}
           onBlur={handleBlur('city')}
           value={values.city}/>
         <Input
-          placeholder="State"
+          label="State"
+          placeholder="West Midlands"
           onChangeText={handleChange('state')}
           onBlur={handleBlur('state')}
           value={values.state}/>
         <Input
-          placeholder="Post Code"
+          label="Post Code"
+          placeholder="NW1 6XE"
           onChangeText={handleChange('postcode')}
           onBlur={handleBlur('postcode')}
           value={values.postcode}/>
         <Input
-          placeholder="Country"
+          label="Country"
+          placeholder="United Kingdom"
           onChangeText={handleChange('country')}
           onBlur={handleBlur('country')}
           value={values.country}/>
         <Input
-          placeholder="Email"
+          label="Email"
+          placeholder="james@gov.uk"
           onChangeText={handleChange('email')}
           onBlur={handleBlur('email')}
           value={values.email}/>
         <Input
-          placeholder="Phone"
+          label="Phone"
+          placeholder="+628123456789"
           onChangeText={handleChange('phone')}
           onBlur={handleBlur('phone')}
           value={values.phone}/>
@@ -87,14 +98,41 @@ const _renderForm = ({ handleCheckoutSubmit }: Props): JSX.Element => (
           onPress={handleSubmit}
           title="Proceed to payment"
         />
-      </View>
+      </>
     )}
   </Formik>
 );
 
+const _renderOrderItem = ({ name, price, quantity }: CartItem): JSX.Element => (
+  <View style={styles.orderItem}>
+    <Text style={{ flex: 0.3 }}>{name}</Text>
+    <Text style={{ flex: 0.3 }}>{toAmount(price)}</Text>
+    <Text style={{ flex: 0.1 }}>x {quantity} =</Text>
+    <Text style={{ flex: 0.3, textAlign: 'right' }}>{toAmount(price * quantity)}</Text>
+  </View>
+);
+
+const _renderOrders = ({ products }: Props) => products.map(_renderOrderItem);
+
+const _renderCheckout = (props: Props): JSX.Element => (
+  <View style={styles.checkoutContainer}>
+    <Text style={styles.textHeading}>Your orders:</Text>
+    {_renderOrders(props)}
+    <View style={styles.orderItem}>
+      <Text style={{ fontWeight: 'bold' }}>Total:</Text>
+      <Text style={{ fontWeight: 'bold' }}>{toAmount(props.total)}</Text>
+    </View>
+    <Text style={styles.textHeading}>Billing details:</Text>
+    {_renderForm(props)}
+  </View>
+);
+
 const Checkout = (props: Props): JSX.Element => (
   <ScrollView>
-    {_renderForm(props)}
+    {props.products.length > 0 ?
+        _renderCheckout(props) :
+        <Text style={styles.textEmpty}>You have not ordered any item</Text>
+    }
   </ScrollView>
 );
 
